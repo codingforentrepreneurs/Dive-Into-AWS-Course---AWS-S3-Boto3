f25e3bf3901129080291e8d1099de9d3f31adff3
Justin Mitchel <hello@teamcfe.com>
2018-12-02 11:14:41 -0800

    4 - Boto 3 Client with Credentials

--- connect.py
+++ connect.py
@@ -1,7 +1,16 @@
 # https://kirr.co/bnk7dx
 import boto3
 
-client = boto3.client('s3')
+AWS_ACCESS_KEY_ID = 'AKIAIPCQFHKC4E4EZ62A' 
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_DEFAULT_REGION = 'us-east-1'
+
+
+client = boto3.client('s3',
+	aws_access_key_id=AWS_ACCESS_KEY_ID,
+	aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+	region_name=AWS_DEFAULT_REGION
+	)
 
 
 response = client.list_buckets()
