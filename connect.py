776875ea105a76e02027404638e2437129638431
Justin Mitchel <hello@teamcfe.com>
2018-12-02 11:41:38 -0800

    6 - Bot 3 Session

--- connect.py
+++ connect.py
@@ -4,7 +4,7 @@ import boto3
 AWS_ACCESS_KEY_ID = 'AKIAIPCQFHKC4E4EZ62A' 
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_DEFAULT_REGION = 'us-east-1'
-
+AWS_BUCKET_NAME = 'aws-cfe-intro'
 
 client = boto3.client('s3',
 	aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -16,3 +16,21 @@ client = boto3.client('s3',
 response = client.list_buckets()
 
 print(response)
+
+session = boto3.Session(
+	aws_access_key_id=AWS_ACCESS_KEY_ID,
+	aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+	region_name=AWS_DEFAULT_REGION
+	)
+
+s3 = session.resource("s3")
+
+for bucket in s3.buckets.all():
+	print(bucket.name)
+
+
+
+
+
+
+
