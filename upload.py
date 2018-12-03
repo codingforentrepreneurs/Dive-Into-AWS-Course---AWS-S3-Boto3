5cb91eef976c2002b1771d199f3e751e84efa4ec
Justin Mitchel <hello@teamcfe.com>
2018-12-03 12:29:03 -0800

    20 - Generate Presigned  Post Data

--- upload.py
+++ upload.py
@@ -1,17 +1,27 @@
 # https://kirr.co/bnk7dx
 import os
-from conf import IMAGE_DIR, bucket
+from conf import IMAGE_DIR, bucket, AWS_BUCKET_NAME, get_s3_client
 from utils import UploadProgressPercentage
 
 file_path = os.path.join(IMAGE_DIR, '1.png')
 key_name = '1.png'
 
 
-bucket.upload_file(file_path, 
-    '1.png', 
-    ExtraArgs={'ACL': 'public-read'},
-    Callback=UploadProgressPercentage(file_path)
-    )
+s3 = get_s3_client()
+
+post_data = s3.generate_presigned_post(
+    Bucket = AWS_BUCKET_NAME,
+    Key = 'upload.png'
+)
+
+print(post_data)
+
+
+# bucket.upload_file(file_path, 
+#     '1.png', 
+#     ExtraArgs={'ACL': 'public-read'},
+#     Callback=UploadProgressPercentage(file_path)
+#     )
 
 
 # with open(file_path, 'rb') as data:
